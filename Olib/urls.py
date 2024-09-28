e675484c9d79495a519b13bccebe81322ad86b4c
Rangat001 <rgtwork114@gmail.com>
2024-09-28 20:35:48 +0530

    App and Pages

--- Olib/urls.py
+++ Olib/urls.py
@@ -1,22 +1,20 @@
-"""
-URL configuration for Olib project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path
+from Home import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+   path("",views.index,name="Home"),
+   path("signup",views.sign_up,name="Sign_up"),
+   path("login",views.log_in,name="Login"),
+   path("logout",views.user_logout,name="logout"),
+   path("forgot",views.forgot_password,name="forgot_password"),
+   path("user_add",views.profile,name="Add_User"),
+   path("get_AllUser",views.getAllUser,name="GET_Users"),
+   path("home",views.index,name="Home"),
+   path("profile",views.profile,name="Profile"),
+
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
